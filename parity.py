0a2698a2ee9a515ce09de3d860c8523267f4237b
sohumgautam17 <69330896+sohumgautam17@users.noreply.github.com>
2023-10-14 14:24:45 -0400

    Add files via upload

---
+++ parity.py
@@ -0,0 +1,23 @@
+#x = int(input("What is x?"))
+
+#if x % y ==0:
+ #   print("x is Even")
+#else:
+  #  print("x is Odd")
+
+def main():
+    x = int(input("What is x?"))
+    if is_even(x):
+        print("X is even")
+    else:
+        print("X is odd")
+        
+        
+def is_even(n):
+    #if n%2 ==0:
+        #return True
+    #else:
+        #return False
+    return True if x % 2 == 0 else return False
+
+main()
