15bc8c71e1e5d11851f012a3d7af63a4cd4b1e09
sohumgautam17 <69330896+sohumgautam17@users.noreply.github.com>
2023-11-01 10:35:08 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,14 +4,13 @@
 # float is decimal point and round it up
 def main():
   x = int(input("What is x"))
-  print("X squared is", square(X)
+  print("X squared is", square(x))
 
-def square():
+def square(n):
   return n*n
 
-if __name__ = "__main__":
-  main()
-
+if __name__ == "__main__":
+    main()
 # or 
 
 # [] signals that something is optional   &     :,   puts , in between every 3 values 
